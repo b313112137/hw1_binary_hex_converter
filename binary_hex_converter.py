1d685432588213a28aae39b61abf658f08c77984
b313112137 <b313112137@tmu.edu.tw>
2025-03-11 20:44:23 +0800

    Update binary_hex_converter.py

--- binary_hex_converter.py
+++ binary_hex_converter.py
@@ -1,7 +1,8 @@
 def convert_number(num):
     if num < 0 or num > 255:
-        return "Error: Input number must be between 0 and 255."
-    x=num
+        return "Error: Input number must be between 0 and 255.", ""
+
+    x = num
     # Convert to binary by checking from the highest power of 2
     binary = ""
     for i in range(7, -1, -1):  # From 2^7 down to 2^0
@@ -10,11 +11,14 @@ def convert_number(num):
             x -= 2**i  # Subtract the corresponding power of 2
         else:
             binary += "0"
+     
+    first_half = binary[:4]  # Take the first four bits
+    second_half = binary[4:] # Take the last four bits
+    hex1 = hex(int(first_half, 2))[2:].upper()
+    hex2 = hex(int(second_half, 2))[2:].upper()
+    hexadecimal = hex1 + hex2
 
-    # Convert to hexadecimal
-    hexadecimal = hex(num)[2:].upper()
-
-    return binary, hexadecimal
+    return binary, hexadecimal  # Return binary and hexadecimal results
 
 def main():
     num = int(input("Enter a number between 0 and 255: "))
@@ -24,5 +28,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
