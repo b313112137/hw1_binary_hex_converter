4cc53740c52242a2a97585bf755090eb185e2789
b313112137 <b313112137@tmu.edu.tw>
2025-03-15 14:45:44 +0800

    Update binary_hex_converter.py

--- binary_hex_converter.py
+++ binary_hex_converter.py
@@ -3,22 +3,27 @@ def convert_number(num):
         return "Error: Input number must be between 0 and 255.", ""
 
     x = num
-    # Convert to binary by checking from the highest power of 2
+   
     binary = ""
-    for i in range(7, -1, -1):  # From 2^7 down to 2^0
+    for i in range(7, -1, -1):  
         if x >= 2**i:
             binary += "1"
-            x -= 2**i  # Subtract the corresponding power of 2
+            x -= 2**i 
         else:
             binary += "0"
-     
-    first_half = binary[:4]  # Take the first four bits
-    second_half = binary[4:] # Take the last four bits
-    hex1 = hex(int(first_half, 2))[2:].upper()
-    hex2 = hex(int(second_half, 2))[2:].upper()
-    hexadecimal = hex1 + hex2
 
-    return binary, hexadecimal  # Return binary and hexadecimal results
+    first_half = binary[:4]  
+    second_half = binary[4:] 
+    def binary_to_decimal(four_bits):
+        return (int(four_bits[0]) * (2**3) +
+                int(four_bits[1]) * (2**2) +
+                int(four_bits[2]) * (2**1) +
+                int(four_bits[3]) * (2**0))
+
+    hex1 = hex(binary_to_decimal(first_half))[2:].upper()
+    hex2 = hex(binary_to_decimal(second_half))[2:].upper()
+    hexadecimal = f"{hex1}{hex2}"
+    return binary, hexadecimal 
 
 def main():
     num = int(input("Enter a number between 0 and 255: "))
